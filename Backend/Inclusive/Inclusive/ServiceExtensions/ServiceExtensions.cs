1af16b083697752dc2a004f17586cfc83bf62391
enano17711 <120424316+enano17711@users.noreply.github.com>
2023-03-27 10:46:55 -0400

    feat: identity framework init functionality

---
+++ Backend/Inclusive/Inclusive/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Identity;
+using Repository;
+
+namespace Inclusive.ServiceExtensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services) {
+            var builder = services.AddIdentity<User, IdentityRole>(opt => {
+                opt.Password.RequiredLength = 5;
+            })
+                .AddEntityFrameworkStores<RepositoryContext>()
+                .AddDefaultTokenProviders();
+        }
+    }
+}
