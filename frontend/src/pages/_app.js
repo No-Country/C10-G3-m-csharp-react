934705c0f2c0216ea1e572c4cb40d26172f308a3
Luis Simosa <simosa37@gmail.com>
2023-03-28 10:26:25 -0300

    style: add sass files & colors partial

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import MainLayout from './components/layout/layout';
-import "@/styles/globals.css";
+import "@/styles/globals.sass";
 
 export default function App({ Component, pageProps }) {
   return (
