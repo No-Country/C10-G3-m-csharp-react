84fdccd09449ddb6f63262b9d5ad30146fad0f15
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-04-03 10:22:27 -0300

    style: button icons re sized

--- frontend/src/components/Buttons/AppleButton.js
+++ frontend/src/components/Buttons/AppleButton.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
-import AppleIcon from '../../../public/apple.png';
+import AppleIcon from '../../../public/img/buttons/apple.png';
 import styles from '../../scss/components/barrel_components.module.scss';
 
 
-const {fastSignInButton} = styles;
+const {fastSignInButton,imgStyled} = styles;
 
 export default function AppleButton() {
   return (
     <button className={fastSignInButton}>
-        <Image src={AppleIcon} alt='GoogleIcon'/>
+        <Image src={AppleIcon} alt='GoogleIcon' className={imgStyled}/>
         <p>Apple</p>
     </button>
   )
