d8370f7899dc4240dd0e1a751fccacbe08034d42
Gerson Flores Narciso <110065310+Gerson12-afk@users.noreply.github.com>
2023-04-04 17:39:51 -0500

    HeaderCambio

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,57 +2,47 @@ import Header from "../../scss/views/Header/Header.module.scss"
 import Image from "next/image"
 import Navbar from "../Navbar/Navbar"
 import Link from "next/link"
+import LogoHeader from "../HeaderLogo/HeaderSvg"
+import LupaHeader from "../LupaSvg/Lupa"
+import MapaHeader from "../MapaSvg/Mapa"
+
 export default function isHeader() {
     return (
         <header className={Header.headerContainer}>
             <div className={Header.Container}>
                 <div className={Header.Container__Dates}>
-                   
-                    <div className={Header.fingers}>
-                        <Image className={Header.fingers2} width={7} height={4.82} src="/images/Vector2.svg" alt="fingers"/>
-                        <Image width={6.5} height={12.14} src="/images/Vector3.svg" alt="fingers"/>
-                        <Image width={6.5} height={16.96} src="/images/Vector4.svg" alt="fingers"/>
-                        <Image width={6.5} height={8.87} src="/images/Vector5.svg" alt="fingers"/>
-                    </div>
-
-                    <div className={Header.love}>
-                        <Image width={40} height={26.32} src="/images/Vector1.svg" alt="love" />
-                    </div>
-
-                    <div className={Header.silla}>
-                        <Image className={Header.cabeza} width={8} height={3.22} src="/images/Vector8.svg" alt="cabeza" />
-                        <Image width={10} height={8.75} src="/images/Vector6.svg" alt="cuerpo" />
-                        <Image className={Header.rueda} width={14} height={10.42} src="/images/Vector7.svg" alt="rueda" />
-                    </div>
-                    <div className={Header.mano}>
-                        <Image width={40} height={28.94} src="/images/Vector9.svg" alt="mano" />
-                        <Image width={55} height={14.23} src="/images/Inclusive.svg" alt="inclusive" />
-
+                    <div>
+                        <LogoHeader/>
                     </div>
                 </div>
                 <div className={Header.Texto}>
                     <h1 className={Header.Texto__Welcome}> Hola Pedro Pascal </h1>
                     <h2 className={Header.Texto__date}>Viernes 24 de marzo</h2>
                 </div>
-               <Navbar/>
+                <Navbar />
             </div>
 
             <form className={Header.form}>
                 <label htmlFor="text">
-                <input 
-                id="text"
-                className={Header.Search} 
-                type="text"
-                 placeholder="Buscar establecimiento"/>
+                    <input
+                        id="text"
+                        className={Header.Search}
+                        type="text"
+                        placeholder="Buscar establecimiento" />
                 </label>
-                <Image className={Header.lupa} width={17.48} height={17.48} src="./images/search.svg" alt="lupa"/>
+                <div className={Header.lupa}>
+                <LupaHeader/>
+                </div>
             </form>
             <div className={Header.mapa}>
-               <Image width={13.97} height={19.97} src="./images/place.svg" alt="mapa"/>
-               <Link href="/" className={Header.mapa__letter}> Buscar en el mapa </Link>
-              </div>
+                <div>
+                <MapaHeader/>
 
-           
+                </div>
+                <Link href="/" className={Header.mapa__letter}> Buscar en el mapa </Link>
+            </div>
+
+
         </header>
 
     )
