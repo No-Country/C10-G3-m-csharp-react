0f18cf309a2f04410d901b9ba0ff0ca31269f700
Gerson Flores Narciso <110065310+Gerson12-afk@users.noreply.github.com>
2023-04-04 08:12:24 -0500

    Organizacion-Header

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Image from "next/image"
+import Link from "next/link";
+
+export default function Navbar() {
+    const [isopen, setIsopen] = useState(false)
+   
+    const Cambio = () => {
+        setIsopen(!isopen)
+    }
+
+      return (
+        <div className="Container">
+            <div className={`Cortina ${isopen && "remove"}`}></div>
+            <div className={`Toggle ${isopen && "open"}`} onClick={Cambio} >
+                <Image className="Hamburger" width={32} height={20} src="./images/Hamburguesa.svg" alt="Hamburguesa" />
+            </div>
+
+            <div className={`items ${isopen && "open"}`}>
+                 
+                <div className={`ContainerClose ${isopen && "open"}`} onClick={Cambio} >
+                    <Image width={18.63} height={18.63} src="./images/close.svg" alt="close" />
+                </div>
+                <ul className="itemsListas">
+                    <li>
+                       <Link href="/"> Perfil de usuario </Link>
+
+                    </li>
+                    <li>
+                        <Link href="/"> Lugares Destacados </Link>
+
+                    </li>
+                    <li>
+                        <Link href="/"> Preguntas Frecuentes </Link>
+
+                    </li>
+                    <li>
+                        <Link href="/"> Registra tu local </Link>
+
+                    </li>
+                    <li className="Ayuda">
+                      <Link href="/"> Ayuda </Link>
+
+                    </li>
+                    
+                    <li>
+                        <Link href="/"> Cerrar sesión </Link>
+                    </li>
+                </ul>
+            </div>
+           
+
+        </div>
+
+    )
+}
