776a65318d18f27fb6b732fbf2061fe480432910
Luis Simosa <simosa37@gmail.com>
2023-04-06 10:08:20 -0300

    fix: add temporary conditional rendering of header

--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -1,11 +1,15 @@
 import Header from "../Header/Header"
-import Navbar from "../Navbar/Navbar";
-
+import {useRouter} from 'next/router';
 
 function MainLayout({ children }) {
+
+  //
+  const router = useRouter();
+  const routes = ["/login", "/signup", "/signIn"]
+
   return (
     <>
-      <Header/>
+      {routes.includes(router.pathname) ? "" : <Header/>}
       <main>{children}</main>
       <footer>footer</footer>
     </>
