2507188172e99eef6c7d35ed140cf1d6a792832e
Luis Simosa <simosa37@gmail.com>
2023-04-13 10:43:29 -0300

    feat: add landing intro

--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -3,6 +3,7 @@ import styles from "../../scss/views/Landing/Landing.module.scss";
 import Image from "next/image";
 import InclusiveLogo from "@/components/inclusiveLogo/inclusiveLogo";
 import Img1 from "../../../public/img/landing/landing-img-1.png";
+import Img2 from "../../../public/img/landing/landing-img-2.png";
 import SearchLogo from "@/components/searchLogo/searchLogo";
 
 function Landing() {
@@ -48,12 +49,38 @@ function Landing() {
           />
           <div className={styles.form}>
             <input type="text" placeholder="Buscar lugares accesibles" />
-            <SearchLogo/>
+            <SearchLogo />
           </div>
 
           <h1>Bienvenidos a INCLUSIVE</h1>
         </section>
-        <section></section>
+
+        <section className={styles.intro}>
+          <div className={styles.intro_img}>
+            <Image
+              src={Img2}
+              width={312}
+              height={200}
+              alt="people working"
+              priority
+            />
+          </div>
+          <div className={styles.intro_text}>
+            <h2>
+              Accesabilidad
+              <br /> para todos
+            </h2>
+            <p>
+              En <strong>Inclusive</strong>, nuestra misión es hacer que la accesibilidad sea más
+              accesible. Con nuestra aplicación, puedes encontrar los lugares
+              más accesibles para ti con un solo clic. Bienvenidos a <strong>Inclusive</strong>,
+              donde la accesibilidad está al alcance de todos.
+            </p>
+
+            <button>Conéctate</button>
+          </div>
+        </section>
+
         <section></section>
         <section></section>
         <section></section>
