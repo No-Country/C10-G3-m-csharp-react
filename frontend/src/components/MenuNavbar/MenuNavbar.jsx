71f05bcc8ae12a6a0a67c506cec5e62ac5853f47
Luis Simosa <simosa37@gmail.com>
2023-04-19 14:13:23 -0300

    feat: add date to header

--- frontend/src/components/MenuNavbar/MenuNavbar.jsx
+++ frontend/src/components/MenuNavbar/MenuNavbar.jsx
@@ -3,6 +3,11 @@ import Navbar from "../Navbar/Navbar";
 import Header from "../../scss/views/Header/Header.module.scss"
 
 export default function MenuNavbar(){
+
+    const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    const mes = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre',]
+    const date = new Date()
+
     return(
         <>        
         <div className={Header.Container}>
@@ -13,7 +18,7 @@ export default function MenuNavbar(){
         </div>
         <div className={Header.Texto}>
             <h1 className={Header.Texto__Welcome}> Hola Pedro Pascal </h1>
-            <h2 className={Header.Texto__date}>Viernes 24 de marzo</h2>
+            <h2 className={Header.Texto__date}>{`${dias[date.getDay()]} ${date.getDay()} de ${mes[date.getMonth()]}`}</h2>
         </div>
         <Navbar/>
     </div>
